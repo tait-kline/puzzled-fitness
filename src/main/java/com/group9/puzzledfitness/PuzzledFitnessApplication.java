1e42a4cea326ce1353432b19f4a11aa748d28d08
Tait Kline <tkline201@gmail.com>
2022-11-01 13:00:49 -0400

    Initial commit

---
+++ src/main/java/com/group9/puzzledfitness/PuzzledFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.group9.puzzledfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzledFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzledFitnessApplication.class, args);
+	}
+
+}

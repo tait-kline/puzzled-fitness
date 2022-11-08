ea7aa434a544ba4f4fb121185018579997658bae
Ethan Zhao <etz2@pitt.edu>
2022-11-07 23:58:12 -0500

    Added image and login prompt for user

--- PuzzledFitness/src/app/login/login.component.ts
+++ PuzzledFitness/src/app/login/login.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  username = "";
+  password = "";
+  errorMsg = "";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    if(this.username.trim().length === 0){
+      this.errorMsg = "Username is required";
+    }
+    else if(this.password.trim().length === 0){
+      this.errorMsg = "Password is required";
+    }
+    else{
+      this.errorMsg = "";
+    }
   }
 
 }
